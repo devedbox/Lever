af831457022c67c189e20f32ba246facf07b2cc7
devedbox <862099730@qq.com>
2018-08-12 19:47:55 +0800

    feat: Adds task management.

--- Sources/Lever/Types/Session.swift
+++ Sources/Lever/Types/Session.swift
@@ -7,12 +7,29 @@
 
 import Foundation
 
-public class Session: NSObject {
+public class Session: NSObject, SessionRepresentable {
+    private struct _SharedToken {
+        static let instance = Session(config: .default)
+    }
+    
+    public class var shared: Session { return _SharedToken.instance }
     
     private let _session: URLSession
-    internal var sessionDelegate: SessionDelegate? {
-        return _session.delegate as? SessionDelegate
+    public var session: URLSession { return _session }
+    
+    private var _tasks: [TaskRepresentable] {
+        return _dataTasks as [TaskRepresentable] + _downloadTasks as [TaskRepresentable] + _uploadTasks as [TaskRepresentable] + _streamTasks as [TaskRepresentable]
     }
+    private var _dataTasks: [DataTaskRepresentable] = []
+    private var _downloadTasks: [DownloadTaskRepresentable] = []
+    private var _uploadTasks: [UploadTaskRepresentable] = []
+    private var _streamTasks: [StreamTaskRepresentable] = []
+    
+    private let _sessionQueue = DispatchQueue(label: "com.session.lever")
+    internal weak var sessionDelegate: SessionDelegate?
+    
+    public private(set) var invalidHandler: ((Swift.Error?) -> Void)?
+    public private(set) var challengeHandler: ((URLAuthenticationChallenge, (URLSession.AuthChallengeDisposition, URLCredential?) -> Void) -> Void)?
     
     public init(session: URLSession) {
         _session = session
@@ -20,11 +37,25 @@ public class Session: NSObject {
 }
 
 extension Session {
-    public convenience init(config: URLSessionConfiguration,
-                            delegate: URLSessionDelegate? = nil,
-                            delegateQueue: OperationQueue? = nil)
+    public convenience init(
+        config: URLSessionConfiguration,
+        delegate: URLSessionDelegate? = nil,
+        delegateQueue: OperationQueue? = nil)
     {
-        self.init(session: .init(configuration: config, delegate: delegate ?? SessionDelegate(), delegateQueue: delegateQueue))
+        let sessionDelegate = SessionDelegate()
+        self.init(session: .init(configuration: config, delegate: delegate ?? sessionDelegate, delegateQueue: delegateQueue))
+        
+        if case .none = delegate {
+            sessionDelegate.sessionExtractor = { [unowned self] in self }
+            sessionDelegate.taskExtractor = { [unowned self] task in self._task(for: task) }
+            sessionDelegate.dataTaskExtractor = { [unowned self] task in self._dataTask(for: task) }
+            sessionDelegate.downloadTaskExtrator = { [unowned self] task in self._downloadTask(for: task) }
+            if #available(OSX 10.11, *) {
+                sessionDelegate.streamTaskExtrator = { [unowned self] task in self._streamTask(for: task) }
+            }
+            sessionDelegate.taskDisposer = { [unowned self] task in self._removeTask(for: task) }
+            self.sessionDelegate = sessionDelegate
+        }
     }
 }
 
@@ -37,38 +68,97 @@ extension Session {
 //}
 
 extension Session {
-    public func task(for request: RequestConvertiable) throws -> TaskRepresentable {
-        return _session.dataTask(with: try request.asRequest())
+    public func task(for request: RequestConvertiable) throws -> DataTaskRepresentable {
+        let task = DataTask(session: self, task: _session.dataTask(with: try request.asRequest()))
+        _sessionQueue.sync { [unowned self] in self._dataTasks.append(task) }
+        return task
     }
 }
 
 extension Session {
-    public func download(from request: RequestConvertiable) throws -> TaskRepresentable {
-        return _session.downloadTask(with: try request.asRequest())
+    public func download(from request: RequestConvertiable) throws -> DownloadTaskRepresentable {
+        let task = DownloadTask(session: self, task: _session.downloadTask(with: try request.asRequest()))
+        _sessionQueue.sync { [unowned self] in self._downloadTasks.append(task) }
+        return task
     }
     
-    public func download(with resumeData: Data) throws -> TaskRepresentable {
-        return _session.downloadTask(withResumeData: resumeData)
+    public func download(with resumeData: Data) throws -> DownloadTaskRepresentable {
+        let task = DownloadTask(session: self, task: _session.downloadTask(withResumeData: resumeData))
+        _sessionQueue.sync { [unowned self] in self._downloadTasks.append(task) }
+        return task
     }
 }
 
 extension Session {
-    public func upload(to request: RequestConvertiable, from data: Data) throws -> TaskRepresentable {
-        return _session.uploadTask(with: try request.asRequest(), from: data)
+    public func upload(to request: RequestConvertiable, from data: Data) throws -> UploadTaskRepresentable {
+        let task = UploadTask(session: self, task: _session.uploadTask(with: try request.asRequest(), from: data))
+        _sessionQueue.sync { [unowned self] in self._uploadTasks.append(task) }
+        return task
     }
     
-    public func upload(to request: RequestConvertiable, from file: URLConvertiable) throws -> TaskRepresentable {
-        return _session.uploadTask(with: try request.asRequest(), fromFile: try file.asURL())
+    public func upload(to request: RequestConvertiable, from file: URLConvertiable) throws -> UploadTaskRepresentable {
+        let task = UploadTask(session: self, task: _session.uploadTask(with: try request.asRequest(), fromFile: try file.asURL()))
+        _sessionQueue.sync { [unowned self] in self._uploadTasks.append(task) }
+        return task
     }
 }
 
 // MARK: - Public.
 
 extension Session {
-    @available(macOS 10.11, *)
-    public func allTasks(_ results: @escaping ([Task]) -> Void) {
-        _session.getAllTasks { tasks in
-            results(tasks.map { .init(task: $0) })
+    public var allTasks: [TaskRepresentable] { return _tasks }
+    
+    public func invalid(_ handler: @escaping (Swift.Error?) -> Void) {
+        invalidHandler = handler
+    }
+    
+    public func challenge(_ handler: @escaping (URLAuthenticationChallenge, (URLSession.AuthChallengeDisposition, URLCredential?) -> Void) -> Void) {
+        challengeHandler = handler
+    }
+}
+
+// MARK: - Private.
+
+extension Session {
+    private func _task(for task: URLSessionTask) -> TaskRepresentable? {
+        return _sessionQueue.sync { [unowned self] in self._tasks.first(where: { $0.task == task }) }
+    }
+    
+    private func _dataTask(for task: URLSessionDataTask) -> DataTaskRepresentable? {
+        return _sessionQueue.sync { [unowned self] in self._dataTasks.first(where: { $0.task == task }) }
+    }
+    
+    private func _downloadTask(for task: URLSessionDownloadTask) -> DownloadTaskRepresentable? {
+        return _sessionQueue.sync { [unowned self] in self._downloadTasks.first(where: { $0.task == task }) }
+    }
+    
+    private func _uploadTask(for task: URLSessionUploadTask) -> UploadTaskRepresentable? {
+        return _sessionQueue.sync { [unowned self] in self._uploadTasks.first(where: { $0.task == task }) }
+    }
+    
+    @available(OSX 10.11, *)
+    private func _streamTask(for task: URLSessionStreamTask) -> StreamTaskRepresentable? {
+        return _sessionQueue.sync { [unowned self] in self._streamTasks.first(where: { $0.task == task }) }
+    }
+    
+    @discardableResult
+    private func _removeTask(for task: URLSessionTask) -> TaskRepresentable? {
+        return _sessionQueue.sync { [unowned self] in
+            switch task {
+            case let task as URLSessionUploadTask:
+                return self._uploadTasks.firstIndex(where: { $0.task == task }).flatMap { self._uploadTasks.remove(at: $0) }
+            case let task as URLSessionDownloadTask:
+                return self._downloadTasks.firstIndex(where: { $0.task == task }).flatMap { self._downloadTasks.remove(at: $0) }
+            case let task as URLSessionDataTask:
+                return self._dataTasks.firstIndex(where: { $0.task == task }).flatMap { self._dataTasks.remove(at: $0) }
+            default:
+                if #available(OSX 10.11, *) {
+                    if let task = task as? URLSessionStreamTask {
+                        return self._streamTasks.firstIndex(where: { $0.task == task }).flatMap { self._streamTasks.remove(at: $0) }
+                    }
+                }
+                return nil
+            }
         }
     }
 }
