9100d3ae5ce84a58cf698d9031e931c6f9e9054c
devedbox <862099730@qq.com>
2018-08-12 19:47:18 +0800

    feat: Adds URLEncodable file.

---
+++ Sources/Lever/Protocols/URLEncodable.swift
@@ -0,0 +1,22 @@
+//
+//  URLEncodable.swift
+//  Lever
+//
+//  Created by devedbox on 2018/8/11.
+//
+
+import Foundation
+
+//public protocol URLEncoder {
+//    func encode(url: URLConvertiable, with params: [AnyHashable: Any]) throws -> URLConvertiable
+//}
+//
+//public protocol URLEncodable {
+//
+//}
+//
+//extension URLEncodable {
+//    public func encode(contents: [AnyHashable: Any], with encoder: URLEncoder) throws -> URLConvertiable {
+//        return try encoder
+//    }
+//}
