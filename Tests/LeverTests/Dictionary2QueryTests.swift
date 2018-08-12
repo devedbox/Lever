f908ba7783c16136a5be51264cfc1a58988b5d28
devedbox <862099730@qq.com>
2018-08-12 20:10:55 +0800

    test: Adds test codes.

---
+++ Tests/LeverTests/Dictionary2QueryTests.swift
@@ -0,0 +1,20 @@
+//
+//  Dictionary2QueryTests.swift
+//  LeverTests
+//
+//  Created by devedbox on 2018/8/11.
+//
+
+import XCTest
+@testable import Lever
+
+class Dictionary2QueryTests: XCTestCase {
+    static var allTests = [
+        ("testSimpleAsQuery", testSimpleAsQuery),
+    ]
+    
+    func testSimpleAsQuery() {
+        let dic = ["loc": 1, "subtype": 2, "type": "typeVal", "anotherType": ["这是个中文", "This is a message."]] as [String : Any]
+        print(try! dic.asQuery())
+    }
+}
--- Tests/LeverTests/LeverTests.swift
+++ Tests/LeverTests/LeverTests.swift
@@ -6,9 +6,27 @@ final class LeverTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(Lever().text, "Hello, World!")
+        // XCTAssertEqual(Lever().text, "Hello, World!")
+        let data: [String: String] = ["sasa": "1", "1": "sasa"]
+        let json = try? JSONEncoder().encode(data)
+        print(String(data: json!, encoding: .utf8)!)
     }
 
+    func testRequest() {
+        let exp = expectation(description: "resp")
+        exp.expectedFulfillmentCount = 2
+        let task = try? Lever.Session.shared.task(for: "https://api.github.com/repos/apple/swift/releases")
+        task?.response { response, completion in
+            print(response)
+            completion(.allow)
+            exp.fulfill()
+        }
+        task?.receiveData { data in
+            print(String(data: data, encoding: .utf8) ?? "")
+            exp.fulfill()
+        }
+        wait(for: [exp], timeout: 120.0)
+    }
 
     static var allTests = [
         ("testExample", testExample),
